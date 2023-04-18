ea966acef9504adb0c8fac1df30ea322b38737c3
Shivam <monkeydeluffy98@icloud.com>
2023-04-18 15:22:35 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+console.log('hello mottorolarolarolarolarola')
+console.log('hello mottorolarolarolarolarola')
+console.log('hello mottorolarolarolarolarola')
+console.log('hello mottorolarolarolarolarola')
+console.log('hello mottorolarolarolarolarola')
+console.log('hello mottorolarolarolarolarolarola')
+console.log('hello mottorolarolarolarolarolarola')
+
+for (let i = 0; i < 10; i++) {
+  if (i % 2 === 0) {
+    setTimeout(() => console.log(`s-${i}`), 1000)
+  } else {
+    console.log('ooh no')
+  }
+}
+
+console.log('HELLO mottorolarolarolarolarolarola')
